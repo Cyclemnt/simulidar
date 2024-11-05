224fce732bc72fbb12c62537d38a76cd5cde34e1
SimonlaF <simonferrier@icloud.com>
2024-11-05 11:31:28 +0100

    Affichage de l'environnement dans une fenêtre séparée avec openCV

--- src/environment.cpp
+++ src/environment.cpp
@@ -89,23 +89,28 @@ bool Environment::isCellFree(int x, int y, double robotDiameter) const {
 
 // Fonction pour afficher la grille
 void Environment::printGrid() const {    
-
-    for (int y = height - 1; y >= 0; --y) {  // Parcourt les lignes de bas en haut
-        for (int x = 0; x < width; ++x) {    // Parcourt chaque colonne de gauche à droite
-            switch (grid[x][y])
-            {
-            case CellState::Wall:
-                std::cout << "█";
-                break;
-            case CellState::Free:
-                std::cout << " ";
-                break;
-
-            default:
-                break;
+    // Création de l'image OpenCV (CV_8UC3 pour une image couleur)
+    cv::Mat CopieGrid(height,width, CV_8UC3);
+ 
+    // Remplissage de CopieGrid en fonction des valeurs de grid
+    for (int y = 0 ; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            cv::Vec3b& pixel = CopieGrid.at<cv::Vec3b>(height-1-y, x);
+            switch (grid[x][y]) {
+                case CellState::Wall:
+                    pixel = cv::Vec3b(0, 0, 0);  // Noir pour Wall
+                    break;
+                case CellState::Free:
+                    pixel = cv::Vec3b(255, 255, 255);  // Blanc pour les cases libres
+                    break;                    
+                case CellState::Unknown:
+                    pixel = cv::Vec3b(127, 127, 127);  // Gris pour les cases inconnues
+                    break;
+                default:
+                    pixel = cv::Vec3b(127, 127, 127);  // Gris pour les cases non définies 
+                    break;
             }
         }
-        std::cout << std::endl;
     }
 }
 
