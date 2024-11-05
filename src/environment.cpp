c73d6ecdc609fbca55c69f1f2e3d4c497eff7ecf
Cyclemnt <clementlamouller@gmail.com>
2024-11-05 14:11:18 +0100

    Changement des couleurs d'affichage

--- src/environment.cpp
+++ src/environment.cpp
@@ -90,33 +90,33 @@ bool Environment::isCellFree(int x, int y, double robotDiameter) const {
 // Fonction pour afficher la grille
 void Environment::printGrid() const {    
     // Création de l'image OpenCV (CV_8UC3 pour une image couleur)
-    cv::Mat CopieGrid(height,width, CV_8UC3);
+    cv::Mat gridCopy(height, width, CV_8UC3);
  
     // Remplissage de CopieGrid en fonction des valeurs de grid
     for (int y = 0 ; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            cv::Vec3b& pixel = CopieGrid.at<cv::Vec3b>(height-1-y, x);
+            cv::Vec3b& pixel = gridCopy.at<cv::Vec3b>(height - 1 - y, x);
             switch (grid[x][y]) {
                 case CellState::Wall:
-                    pixel = cv::Vec3b(0, 0, 0);  // Noir pour Wall
+                    pixel = cv::Vec3b(255, 0, 0);      // Bleu pour les murs
                     break;
                 case CellState::Free:
                     pixel = cv::Vec3b(255, 255, 255);  // Blanc pour les cases libres
                     break;                    
                 case CellState::Unknown:
-                    pixel = cv::Vec3b(127, 127, 127);  // Gris pour les cases inconnues
+                    pixel = cv::Vec3b(0, 0, 0);        // Noir pour les cases inconnues
                     break;
                 default:
-                    pixel = cv::Vec3b(127, 127, 127);  // Gris pour les cases non définies 
+                    pixel = cv::Vec3b(247, 0, 248);    // Rose pour les cases non définies 
                     break;
             }
         }
     }
     // Agrandissement pour visualisation
-    cv::resize(CopieGrid, CopieGrid, cv::Size(width * 50, height * 50), 0, 0, cv::INTER_NEAREST);
+    cv::resize(gridCopy, gridCopy, cv::Size(width * 50, height * 50), 0, 0, cv::INTER_NEAREST);
 
     // Afficher l'image
-    cv::imshow("Affichage de la grille", CopieGrid);
+    cv::imshow("Affichage de l'environnement", gridCopy);
     cv::waitKey(0);
 }
 
